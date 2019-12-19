3ed27c8f4853a5b2f42d3ae97c8d3aefc5c2a2a4
WiktorG <w.grudzina@gmail.com>
2019-12-19 14:54:05 +0100

    test

--- src/main/java/pl/edu/utp/behaviour/AvoidingIntention.java
+++ src/main/java/pl/edu/utp/behaviour/AvoidingIntention.java
@@ -4,7 +4,6 @@ import pl.edu.utp.com.GridObject;
 import pl.edu.utp.gui.Point;
 
 public class AvoidingIntention implements Intention {
-    int x;
     public Point setIntention(GridObject obj) {
         return new Point(0, 0);
     }
