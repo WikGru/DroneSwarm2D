6eda26ffdd5c447d81d7a3dfbf22942d3c8c4026
WiktorG <w.grudzina@gmail.com>
2019-12-19 14:53:05 +0100

    Add 2 classes of intention, Add choosing behaviour from GUI (default, avoiding - not yet implemented), Fix ID drawing on every entity (including finishZones), Fix scaling drones to cell size.

--- src/main/java/pl/edu/utp/behaviour/AvoidingIntention.java
+++ src/main/java/pl/edu/utp/behaviour/AvoidingIntention.java
@@ -4,6 +4,7 @@ import pl.edu.utp.com.GridObject;
 import pl.edu.utp.gui.Point;
 
 public class AvoidingIntention implements Intention {
+    int x;
     public Point setIntention(GridObject obj) {
         return new Point(0, 0);
     }
